993af5ddfa8e80efaf2ce157f8b5b04850a094bf
Shams230514 <84643634+Shams230514@users.noreply.github.com>
2025-05-15 17:37:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -233,4 +233,4 @@ def health():
 
 if __name__ == '__main__':
     print("Démarrage du serveur Flask...")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
